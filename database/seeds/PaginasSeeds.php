7c4bca2b510e309525fdcaaad310d2a53cfdbc24
Sandro Torres <sandrotorres.ti@gmail.com>
2018-06-14 05:05:27 -0300

    Definindo rota, controle e view da página sobre

--- database/seeds/PaginasSeeds.php
+++ database/seeds/PaginasSeeds.php
@@ -23,7 +23,8 @@ class PaginasSeeds extends Seeder
         $paginaSobre->titulo = "Título da Empresa";
         $paginaSobre->descricao = "Descrição breve sobre a empresa";
         $paginaSobre->texto = "Texto sobre a empresa";
-        $paginaSobre->imagem = "site/img/modelo_img_home.jpg";
+        $paginaSobre->imagem = "img/modelo_img_home.jpg";
+        $paginaSobre->mapa= '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14682.45129722063!2d-52.46253585!3d-23.074652!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94929661c8720ce5%3A0x8f6da5f50bf3a4ce!2sShopping+Cidade+Paranava%C3%AD!5e0!3m2!1spt-BR!2sbr!4v1528963196840" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>';
         $paginaSobre->tipo = "sobre";
 
         $paginaSobre->save();
--- resources/views/site/sobre.blade.php
+++ resources/views/site/sobre.blade.php
@@ -1,21 +1,27 @@
-@extends('layouts.site')
-
+@extends('layouts.site') 
 @section('content')
 <div class="container">
 	<div class="row section">
 		<h3 align="center">Sobre</h3>
 		<div class="divider"></div>
 	</div>
+
 	<div class="row section">
 		<div class="col s12 m6">
-			<img class="responsive-img" src="{{ asset('img/modelo_img_home.jpg') }}"></img>
+			@if(isset($pagina->mapa))
+				<div class="video-container">
+					{!! $pagina->mapa !!}
+				</div>
+			@else
+				<img class="responsive-img" src="{{ asset($pagina->imagem) }}"></img>
+			@endif
 		</div>
+
 		<div class="col s12 m6">
 			<h4>{{ $pagina->titulo }}</h4>
-			<blockquote>Descrição breve sobre a empresa.</blockquote>
-			<p>Texto maior sobre a empresa.</p>
+			<blockquote>{{ $pagina->descricao }}</blockquote>
+			<p>{{ $pagina->texto }}</p>
 		</div>
 	</div>
-
 </div>
 @endsection
